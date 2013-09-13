98b2bd565661e8d295defc4d88154dc6893f6638
HawkDaght <hawkdaght@gmail.com>
2013-09-13 18:14:22 -0400

    prototype

---
+++ Prototype/Assets/Script/Char/CharMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMove : MonoBehaviour {
+	Transform myTr;
+	CharacterController myController;
+	Vector3 moveDirection;
+	float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		myTr = transform;
+		myController = GetComponent<CharacterController>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		myTr.position = new Vector3(myTr.position.x,0,myTr.position.z);
+		
+		myController.Move(moveDirection * Time.deltaTime * speed);
+	}
+}
---
+++ Prototype/Assets/Script/Turret/Missile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+	Transform myTr;
+	float speed = 15f;
+	public Transform explosionPrefab;
+	// Use this for initialization
+	void Start () {
+		myTr = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTr.Translate(Vector3.forward * speed * Time.deltaTime);
+	
+	}
+	
+	void OnTriggerEnter(Collider col){
+		//if(col.gameObject.tag == "Player"){
+			Destroy(gameObject);
+			Transform explosion = Instantiate(explosionPrefab,myTr.position,myTr.rotation) as Transform;
+		//}
+	}
+}
